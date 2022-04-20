d4680316bcd2716559c5191140edabb5b7d6facb
Daniel <contato.daniel.dev@gmail.com>
2022-04-19 23:53:41 -0300

    tags condicionais e posts populares

--- header.php
+++ header.php
@@ -69,83 +69,97 @@
                             <?php endif ?>
                         </div>
                     </div>
-                    <div class="main-info">
-                        <div class="row">
-                            <div class="col-md-6 col-xl-8 ramdom-post">
-                                <strong class="me-5">Já viu?</strong>
 
-                                <?php
-                                    $dm_query = new WP_Query([
-                                        "posts_per_page" => 1,
-                                        "post_type" => "post",
-                                        "orderby" => "rand"
-                                    ]);
-                                ?>
+                    <?php if(is_home()): // is_single, verifica se é um post, is_page, se está dentro de uma página, is_home, se é a página home, is_search, se está dentro da página de busca ?>
+                        <div class="main-info">
+                            <div class="row">
+                                <div class="col-md-6 col-xl-8 ramdom-post">
+                                    <strong class="me-5">Já viu?</strong>
 
-                                <?php if($dm_query->have_posts()): ?>
-                                    <?php while($dm_query->have_posts()): ?>
-                                        <?php $dm_query->the_post(); ?>
-                                        <a href="<?=the_permalink()?>">
-                                            <?=the_title()?>
-                                        </a>
-                                    <?php endwhile ?>
-                                    <?php wp_reset_postdata(); ?>
-                                <?php endif ?>
+                                    <?php if(function_exists("wpp_get_mostpopular")): ?>
 
-                            </div>
-                            <div class="col-md-6 col-xl-4 social-area">
-                                <div class="social-text">SIGA-NOS</div>
-                                <div class="social-icons">
+                                            <?php wpp_get_mostpopular([
+                                                'limit' => 1,
+                                                "wpp_start" => "",
+                                                "wpp_end" => "",
+                                                "post_html" => "<a href='{url}'>{text_title}</a>"
+                                            ]); ?>
 
-                                    <?php if(get_theme_mod("dm_facebook")): ?>
-                                        <a href="<?= get_theme_mod("dm_facebook") ?>" target="_blank">
-                                            <img
-                                                src="<?= get_template_directory_uri() ?>/assets/images/facebook.png"
-                                                alt="facebook-icon"
-                                            />
-                                        </a>
-                                    <?php endif ?>
-                                    
-                                    <?php if(get_theme_mod("dm_googleplus")): ?>
-                                        <a href="<?= get_theme_mod("dm_googleplus") ?>" target="_blank">
-                                            <img
-                                                src="<?= get_template_directory_uri() ?>/assets/images/gplus.png"
-                                                alt="google-plus-icon"
-                                            />
-                                        </a>
-                                    <?php endif ?>
-                                    
-                                    <?php if(get_theme_mod("dm_instagram")): ?>
-                                        <a href="<?= get_theme_mod("dm_instagram") ?>" target="_blank">
-                                            <img
-                                                src="<?= get_template_directory_uri() ?>/assets/images/instagram.png"
-                                                alt="instagram-icon"
-                                            />
-                                        </a>
-                                    <?php endif ?>
-                                    
-                                    <?php if(get_theme_mod("dm_twitter")): ?>
-                                        <a href="<?= get_theme_mod("dm_twitter") ?>" target="_blank">
-                                            <img
-                                                src="<?= get_template_directory_uri() ?>/assets/images/twitter.png"
-                                                alt="twitter-icon"
-                                            />
-                                        </a>
-                                    <?php endif ?>
-                                    
-                                    <?php if(get_theme_mod("dm_youtube")): ?>
-                                        <a href="<?= get_theme_mod("dm_youtube") ?>" target="_blank">
-                                            <img
-                                                src="<?= get_template_directory_uri() ?>/assets/images/youtube.png"
-                                                alt="youtube-icon"
-                                            />
-                                        </a>
+                                    <?php else: ?>
+
+                                        <?php $dm_query = new WP_Query([
+                                            "posts_per_page" => 1,
+                                            "post_type" => "post",
+                                            "orderby" => "rand"
+                                        ]); ?>
+                                        
+                                        <?php if($dm_query->have_posts()): ?>
+                                            <?php while($dm_query->have_posts()): ?>
+                                                <?php $dm_query->the_post(); ?>
+                                                <a href="<?=the_permalink()?>">
+                                                    <?=the_title()?>
+                                                </a>
+                                            <?php endwhile ?>
+                                            <?php wp_reset_postdata(); ?>
+                                        <?php endif ?>
+
                                     <?php endif ?>
 
                                 </div>
+                                <div class="col-md-6 col-xl-4 social-area">
+                                    <div class="social-text">SIGA-NOS</div>
+                                    <div class="social-icons">
+
+                                        <?php if(get_theme_mod("dm_facebook")): ?>
+                                            <a href="<?= get_theme_mod("dm_facebook") ?>" target="_blank">
+                                                <img
+                                                    src="<?= get_template_directory_uri() ?>/assets/images/facebook.png"
+                                                    alt="facebook-icon"
+                                                />
+                                            </a>
+                                        <?php endif ?>
+                                        
+                                        <?php if(get_theme_mod("dm_googleplus")): ?>
+                                            <a href="<?= get_theme_mod("dm_googleplus") ?>" target="_blank">
+                                                <img
+                                                    src="<?= get_template_directory_uri() ?>/assets/images/gplus.png"
+                                                    alt="google-plus-icon"
+                                                />
+                                            </a>
+                                        <?php endif ?>
+                                        
+                                        <?php if(get_theme_mod("dm_instagram")): ?>
+                                            <a href="<?= get_theme_mod("dm_instagram") ?>" target="_blank">
+                                                <img
+                                                    src="<?= get_template_directory_uri() ?>/assets/images/instagram.png"
+                                                    alt="instagram-icon"
+                                                />
+                                            </a>
+                                        <?php endif ?>
+                                        
+                                        <?php if(get_theme_mod("dm_twitter")): ?>
+                                            <a href="<?= get_theme_mod("dm_twitter") ?>" target="_blank">
+                                                <img
+                                                    src="<?= get_template_directory_uri() ?>/assets/images/twitter.png"
+                                                    alt="twitter-icon"
+                                                />
+                                            </a>
+                                        <?php endif ?>
+                                        
+                                        <?php if(get_theme_mod("dm_youtube")): ?>
+                                            <a href="<?= get_theme_mod("dm_youtube") ?>" target="_blank">
+                                                <img
+                                                    src="<?= get_template_directory_uri() ?>/assets/images/youtube.png"
+                                                    alt="youtube-icon"
+                                                />
+                                            </a>
+                                        <?php endif ?>
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    <?php endif ?>
                 </div>
             </div>
         </div>
